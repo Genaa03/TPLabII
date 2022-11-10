651d60ec9a1042f9fbaffe258be7ae4546b61abf
Genaa03 <gena_freccia@outlook.com>
2022-11-09 21:32:54 -0300

    finish

--- LabParte3/LabParte3/Presentaciones/VentanaPrincipal.Designer.cs
+++ LabParte3/LabParte3/Presentaciones/VentanaPrincipal.Designer.cs
@@ -42,7 +42,10 @@
             this.consulta8ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.autoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -53,7 +56,7 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(802, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(538, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -90,56 +93,56 @@
             // consulta1ToolStripMenuItem
             // 
             this.consulta1ToolStripMenuItem.Name = "consulta1ToolStripMenuItem";
-            this.consulta1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta1ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta1ToolStripMenuItem.Text = "Consulta 1";
             this.consulta1ToolStripMenuItem.Click += new System.EventHandler(this.consulta1ToolStripMenuItem_Click);
             // 
             // consulta2ToolStripMenuItem
             // 
             this.consulta2ToolStripMenuItem.Name = "consulta2ToolStripMenuItem";
-            this.consulta2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta2ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta2ToolStripMenuItem.Text = "Consulta 2";
             this.consulta2ToolStripMenuItem.Click += new System.EventHandler(this.consulta2ToolStripMenuItem_Click);
             // 
             // consulta3ToolStripMenuItem
             // 
             this.consulta3ToolStripMenuItem.Name = "consulta3ToolStripMenuItem";
-            this.consulta3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta3ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta3ToolStripMenuItem.Text = "Consulta 3";
             this.consulta3ToolStripMenuItem.Click += new System.EventHandler(this.consulta3ToolStripMenuItem_Click);
             // 
             // consulta4ToolStripMenuItem
             // 
             this.consulta4ToolStripMenuItem.Name = "consulta4ToolStripMenuItem";
-            this.consulta4ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta4ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta4ToolStripMenuItem.Text = "Consulta 4";
             this.consulta4ToolStripMenuItem.Click += new System.EventHandler(this.consulta4ToolStripMenuItem_Click);
             // 
             // consulta5ToolStripMenuItem
             // 
             this.consulta5ToolStripMenuItem.Name = "consulta5ToolStripMenuItem";
-            this.consulta5ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta5ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta5ToolStripMenuItem.Text = "Consulta 5";
             this.consulta5ToolStripMenuItem.Click += new System.EventHandler(this.consulta5ToolStripMenuItem_Click);
             // 
             // consulta6ToolStripMenuItem
             // 
             this.consulta6ToolStripMenuItem.Name = "consulta6ToolStripMenuItem";
-            this.consulta6ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta6ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta6ToolStripMenuItem.Text = "Consulta 6";
             this.consulta6ToolStripMenuItem.Click += new System.EventHandler(this.consulta6ToolStripMenuItem_Click);
             // 
             // consulta7ToolStripMenuItem
             // 
             this.consulta7ToolStripMenuItem.Name = "consulta7ToolStripMenuItem";
-            this.consulta7ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta7ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta7ToolStripMenuItem.Text = "Consulta 7";
             this.consulta7ToolStripMenuItem.Click += new System.EventHandler(this.consulta7ToolStripMenuItem_Click);
             // 
             // consulta8ToolStripMenuItem
             // 
             this.consulta8ToolStripMenuItem.Name = "consulta8ToolStripMenuItem";
-            this.consulta8ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consulta8ToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.consulta8ToolStripMenuItem.Text = "Consulta 8";
             this.consulta8ToolStripMenuItem.Click += new System.EventHandler(this.consulta8ToolStripMenuItem_Click);
             // 
@@ -148,22 +151,44 @@
             this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.autoresToolStripMenuItem});
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.ayudaToolStripMenuItem.Text = "Acerca de";
             // 
             // autoresToolStripMenuItem
             // 
             this.autoresToolStripMenuItem.Name = "autoresToolStripMenuItem";
-            this.autoresToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.autoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.autoresToolStripMenuItem.Text = "Autores";
             this.autoresToolStripMenuItem.Click += new System.EventHandler(this.autoresToolStripMenuItem_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(1, 23);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(545, 134);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Bienvenido al sistema academico de las tecnicaturas de la UTN FRC";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "https://www.efundaap.com/wp-content/uploads/2020/02/Logo-UTN-FRC.png";
+            this.pictureBox1.InitialImage = global::LabParte3.Properties.Resources.Logo_UTN_FRC;
+            this.pictureBox1.Location = new System.Drawing.Point(89, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 147);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // VentanaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 542);
+            this.ClientSize = new System.Drawing.Size(538, 333);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "VentanaPrincipal";
@@ -171,6 +196,7 @@
             this.Load += new System.EventHandler(this.VentanaPrincipal_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,6 +218,8 @@
         private System.Windows.Forms.ToolStripMenuItem consulta8ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem autoresToolStripMenuItem;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
